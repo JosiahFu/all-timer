030bea9550b87839668e5530c326ab75b1db2e7d
JosiahFu <josiahfu@gmail.com>
2023-07-22 17:04:00 -0700

    Update icons and manifest

---
+++ src/assets/generate-icons.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+declare -A sizes=(
+    ["favicon.ico"]=48
+    ["icons/android-chrome-192x192.png"]=192
+    ["icons/android-chrome-192x192.png"]=192
+    ["icons/android-chrome-512x512.png"]=512
+    ["icons/apple-touch-icon.png"]=180
+    ["icons/favicon-16x16.png"]=16
+    ["icons/favicon-32x32.png"]=32
+    ["icons/favicon-64x64.png"]=64
+    ["icons/mstile-150x150.png"]=150
+)
+
+for i in "${!sizes[@]}"; do
+    echo "Generating $i"
+    convert -background transparent -size "${sizes[$i]}x${sizes[$i]}" icon.svg "$i"
+done
