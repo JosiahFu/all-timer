1c95b8443c07fffe5af72296de51d59f8279b7f8
JosiahFu <josiahfu@gmail.com>
2023-07-22 17:36:49 -0700

    Update icons

--- src/assets/generate-icons.sh
+++ src/assets/generate-icons.sh
@@ -3,7 +3,6 @@
 declare -A sizes=(
     ["favicon.ico"]=48
     ["icons/android-chrome-192x192.png"]=192
-    ["icons/android-chrome-192x192.png"]=192
     ["icons/android-chrome-512x512.png"]=512
     ["icons/apple-touch-icon.png"]=180
     ["icons/favicon-16x16.png"]=16
@@ -14,5 +13,6 @@ declare -A sizes=(
 
 for i in "${!sizes[@]}"; do
     echo "Generating $i"
-    convert -background transparent -size "${sizes[$i]}x${sizes[$i]}" icon.svg "$i"
+    background=$([[ "$i" == *android* ]] && echo 'white' || echo 'transparent')
+    convert -background $background -size "${sizes[$i]}x${sizes[$i]}" icon.svg "$i"
 done
